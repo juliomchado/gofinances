321eb50a70f08529fa94f55e85b5f3c2acbee7e5
juliomchado <juliocarlos00@hotmail.com>
2021-09-23 01:12:49 -0300

    add/ rn responsive fontsize + header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserInfo,
+    UserWrapper,
+    Avatar,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
 
 export function Dashboard() {
 
     return (
         <Container>
-            <Title>Dashboard</Title>
-        </Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Avatar source={{ uri: 'https://avatars.githubusercontent.com/u/56945282?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Julio</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
+        </Container >
     )
 }
